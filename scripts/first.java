27290ac21d2879012b920d143958387b4d3952d8
Raminder Rathore <raminder.kaur@hcl.com>
2020-09-14 16:21:49 -0400

    java file drafted

---
+++ scripts/first.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;  // Import the Scanner class
+
+class first 
+{
+  public static void main(String[] args) 
+  {
+    Scanner get_inputs = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter username");
+
+    String userName = get_inputs.nextLine();  // Read user input
+    System.out.println("Username is: " + userName);  // Output user input
+  }
+}
