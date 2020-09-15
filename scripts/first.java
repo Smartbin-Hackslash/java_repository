1c6ae11ed19d8036deed66cfc5f6d44a9b50ccc8
Raminder Rathore <raminder.kaur@hcl.com>
2020-09-15 14:19:55 -0400

    retry 7

--- scripts/first.java
+++ scripts/first.java
@@ -4,10 +4,12 @@ class first
 {
   public static void main(String[] args) 
   {
-    Scanner get_inputs = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
+   // Scanner get_inputs = new Scanner(System.in);  // Create a Scanner object
+    //System.out.println("Enter username");
 
-    String userName = get_inputs.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
+    //String userName = get_inputs.nextLine();  // Read user input
+    //System.out.println("Username is: " + userName);  // Output user input
+  System.out.println("Finally");
+
   }
 }
